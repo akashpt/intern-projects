45667101b2f067ba61f4b9a46242b6eb43d71a4f
sowminisn-art <sowminisn7@gmail.com>
2026-05-18 15:51:55 +0530

    Altered a mini project on real time colour detection

--- miniproject.py
+++ miniproject.py
@@ -1,48 +1,50 @@
 import cv2
 import numpy as np
-image = cv2.imread(r"C:\picture\colour.jpg")
-image = cv2.resize(image, (400, 400))
-hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-lower_green = np.array([40,50,50])
-upper_green = np.array([80,255,255])
-green_mask = cv2.inRange(hsv, lower_green, upper_green)
-green_result = cv2.bitwise_and(image, image, mask=green_mask)
-green_mask_bgr = cv2.cvtColor(green_mask, cv2.COLOR_GRAY2BGR)
-cv2.putText(image, "Original Image", (20,20),cv2.FONT_HERSHEY_SIMPLEX,1, (0,0,0), 2)
-cv2.putText(green_mask_bgr, "Green Mask", (20,50),cv2.FONT_HERSHEY_SIMPLEX,1, (0,255,255), 2)
-cv2.putText(green_result, "Green Detection", (20,50),cv2.FONT_HERSHEY_SIMPLEX,1, (0,255,255), 2)
-image_green = cv2.copyMakeBorder(image,10,10,10,10,cv2.BORDER_CONSTANT,value=(255,255,255))
-green_mask_bgr = cv2.copyMakeBorder(green_mask_bgr,10,10,10,10,cv2.BORDER_CONSTANT,value=(0,255,0))
-green_result = cv2.copyMakeBorder(green_result,10,10,10,10,cv2.BORDER_CONSTANT,value=(0,255,0))
-final_green = np.hstack((image_green, green_mask_bgr, green_result))
-lower_blue = np.array([100,150,50])
-upper_blue = np.array([140,255,255])
-blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
-blue_result = cv2.bitwise_and(image, image, mask=blue_mask)
-blue_mask_bgr = cv2.cvtColor(blue_mask, cv2.COLOR_GRAY2BGR)
-cv2.putText(blue_mask_bgr, "Blue Mask", (20,20), cv2.FONT_HERSHEY_SIMPLEX,1, (255,255,255), 2)
-cv2.putText(blue_result, "Blue Detection", (20,20), cv2.FONT_HERSHEY_SIMPLEX,1, (255,255,255), 2)
-image_blue = cv2.copyMakeBorder(image,10,10,10,10,cv2.BORDER_CONSTANT,value=(255,255,255))
-blue_mask_bgr = cv2.copyMakeBorder(blue_mask_bgr,10,10,10,10,cv2.BORDER_CONSTANT,value=(255,255,255))
-blue_result = cv2.copyMakeBorder(blue_result,10,10,10,10, cv2.BORDER_CONSTANT,value=(255,255,255))
-final_blue = np.hstack((image_blue, blue_mask_bgr, blue_result))
-lower_red1 = np.array([0,120,70])
-upper_red1 = np.array([10,255,255])
-lower_red2 = np.array([170,120,70])
-upper_red2 = np.array([180,255,255])
-mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
-mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
-red_mask = mask1 + mask2
-red_result = cv2.bitwise_and(image, image, mask=red_mask)
-red_mask_bgr = cv2.cvtColor(red_mask, cv2.COLOR_GRAY2BGR)
-cv2.putText(red_mask_bgr, "Red Mask", (20,20),cv2.FONT_HERSHEY_SIMPLEX,1, (255,255,255), 2)
-cv2.putText(red_result, "Red Detection", (20,20),cv2.FONT_HERSHEY_SIMPLEX,1, (255,255,255), 2)
-image_red = cv2.copyMakeBorder(image,10,10,10,10,cv2.BORDER_CONSTANT,value=(255,255,255))
-red_mask_bgr = cv2.copyMakeBorder(red_mask_bgr,10,10,10,10,cv2.BORDER_CONSTANT,value=(255,255,255))
-red_result = cv2.copyMakeBorder(red_result,10,10,10,10,cv2.BORDER_CONSTANT,value=(255,255,255))
-final_red = np.hstack((image_red, red_mask_bgr, red_result))
-cv2.imshow("Red Detection Project", final_red)
-cv2.imshow("Blue Detection Project", final_blue)
-cv2.imshow("Green Detection Project", final_green)
+import winsound
+class ColorDetection:
+    def __init__(self, image_path, color_name, lower, upper,lower2=None, upper2=None):
+        self.image = cv2.imread(image_path)
+        self.image = cv2.resize(self.image, (400, 400))
+        self.color_name = color_name
+        self.lower = np.array(lower)
+        self.upper = np.array(upper)
+        self.lower2 = np.array(lower2) if lower2 else None
+        self.upper2 = np.array(upper2) if upper2 else None
+    def detect_color(self):
+        hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
+        mask1 = cv2.inRange(hsv, self.lower, self.upper)
+        if self.lower2 is not None:
+            mask2 = cv2.inRange(hsv, self.lower2, self.upper2)
+            self.mask = mask1 + mask2
+        else:
+            self.mask = mask1
+        self.result = cv2.bitwise_and(self.image,self.image, mask=self.mask)
+        self.mask_bgr = cv2.cvtColor(self.mask, cv2.COLOR_GRAY2BGR)
+        if self.color_name == "Red":
+            print("Danger!Red detected,Something happened")
+            winsound.Beep(1000, 1000)
+            cv2.putText(self.result,"DANGER",(20, 350),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255, 255),2)
+        elif self.color_name == "Amber":
+            cv2.putText(self.result,"WARNING / CAUTION",(20, 350),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 255, 255),2)
+        elif self.color_name == "Green":
+            cv2.putText(self.result,"SAFE CONDITION",(20, 350),cv2.FONT_HERSHEY_SIMPLEX,1,(255, 255, 255),2)
+    def add_text(self):
+        cv2.putText(self.image,"Original Image",(20, 30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0, 0,0),2)
+        cv2.putText(self.mask_bgr,f"{self.color_name} Mask",(20, 30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255,0, 255),2)
+        cv2.putText(self.result,f"{self.color_name} Detection",(20, 30),cv2.FONT_HERSHEY_SIMPLEX,0.7,(255, 255, 255),2)
+    def display(self):
+        img1 = cv2.copyMakeBorder(self.image,10, 10, 10, 10,cv2.BORDER_CONSTANT,value=(255, 255, 255))
+        img2 = cv2.copyMakeBorder(self.mask_bgr,10, 10, 10, 10,cv2.BORDER_CONSTANT,value=(255, 255, 255))
+        img3 = cv2.copyMakeBorder(self.result,10, 10, 10, 10,cv2.BORDER_CONSTANT,value=(255, 255, 255))
+        final = np.hstack((img1, img2, img3))
+        cv2.imshow(f"{self.color_name} Detection", final)
+green = ColorDetection(r"C:\picture\green light.png","Green",[40, 50, 50],[80, 255, 255])
+amber = ColorDetection(r"C:\picture\amber light.png","Amber",[15, 120, 120],[35, 255, 255])
+red = ColorDetection(r"C:\picture\red light.jpg","Red",[0, 120, 70],[10, 255, 255],[170, 120, 70],[180, 255, 255])
+objects = [green, amber, red]
+for obj in objects:
+    obj.detect_color()
+    obj.add_text()
+    obj.display()
 cv2.waitKey(0)
 cv2.destroyAllWindows() 
